fa6e78faba2565c76d69f77e2ed95c9d788bf91a
Lukasz Mielewczyk <lmielewczyk@sigma.ug.edu.pl>
2016-03-23 02:01:59 +0100

    walidacja

--- app/models/corpse.rb
+++ app/models/corpse.rb
@@ -2,4 +2,8 @@ class Corpse < ApplicationRecord
 	def self.search(search)
   		where("first_name LIKE ? or last_name LIKE ? or pesel LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
 	end
+
+	validates :first_name, :last_name, :pesel, :date_of_birth, :date_of_death, presence: true
+	validates :first_name, :last_name, length: { minimum: 3 }
+	validates :pesel, length: { is: 11 }, numericality: true
 end
